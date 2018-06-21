043bcb79ebcd333ae5eeb524533708b5f01410de
shunak <shuna966@gmail.com>
2018-06-21 23:33:53 +0900

    Update indexer.cs

--- indexer.cs
+++ indexer.cs
@@ -2,5 +2,27 @@
 // like an array,  we can use index for class instances
 
 
+using System;
 
+// インデクサ
+
+class Team {
+  private string[] members = new string[3];
+  public string this[int i] {
+    get { return this.members[i]; }
+    set { this.members[i] = value; }
+  }
+}
+
+class MyApp {
+
+  static void Main() {
+    Team giants = new Team();
+    giants[0] = "taguchi";
+    giants[1] = "fkoji";
+    giants[2] = "dotinstall";
+    Console.WriteLine(giants[1]);
+  }
+
+}
 
