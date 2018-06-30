82525d15bc885b642ae2445553c0f8bc82202c54
shunak <shuna966@gmail.com>
2018-06-30 16:42:32 +0900

    Update dictionary.cs

--- dictionary.cs
+++ dictionary.cs
@@ -10,12 +10,12 @@ class MyApp {
 
   static void Main() {
     Dictionary<string, int> users = new Dictionary<string, int>() { // keytype string valuetype int
-      {"taguchi", 50},
-      {"fkoji", 80},
+      {"foo", 50},
+      {"baa", 80},
     };
-    users.Add("dotinstall", 40);
-    Console.WriteLine(users["fkoji"]); // 80
-    users["taguchi"] = 60;
+    users.Add("hoge", 40);
+    Console.WriteLine(users["baa"]); // 80
+    users["foo"] = 60;
     foreach (KeyValuePair<string, int> user in users) {
       Console.WriteLine($"{user.Key}: {user.Value}");
     }
